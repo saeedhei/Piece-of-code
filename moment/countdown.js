8bcac0371214e2c2a955b72513875a11e0034818
Sedric Heidarizarei <saeedhei@gmail.com>
2019-08-07 17:07:14 +0430

    web added

--- moment/countdown.js
+++ moment/countdown.js
@@ -14,3 +14,18 @@ const div = years + ' years ' + days + ' days ' + hrs + ' hrs ' + mins + ' mins 
 console.log(div);
 
 console.log('New York current time is: ' + live.format('ddd YYYY-MM-DD hh:mm:ss'));
+
+//web
+var diffTime = moment('2016-06-13T00:00:00+08:00')
+	.diff( '2016-06-13T00:00:00+00:00');
+var duration = moment.duration(diffTime);
+var years = duration.years(),
+	days = duration.days(),
+	hrs = duration.hours(),
+  mins = duration.minutes(),
+  secs = duration.seconds();
+
+var div = document.createElement('div');
+div.innerHTML = years + ' years ' + days + ' days ' + hrs + ' hrs ' + mins + ' mins ' + secs + ' sec';
+document.body.appendChild(div);
+
