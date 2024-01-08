13a4f85dd41feffa231b0b4e85ed4cf2cd098896
Saeed Heidarizarei <saeedhei@gmail.com>
2024-01-08 11:24:58 +0100

    Create string.vue

---
+++ vue/pinia/string.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import { useStringStore } from '@/stores/string'
+const store = useCounterStore()
+const stringStore = useStringStore()
+
+
+function increment(){
+  store.count++
+}
+function decrement(){
+  store.count--
+}
+
+function updateString(){
+  stringStore.string = 'Updated string'; // Update the string value
+  stringStore.updateString(stringStore.string); // Update the store value
+  stringStore.updateString("Cool");
+}
+</script>
+
+<template>
+  <div>
+    <button @click="increment">+</button> 
+    Count Value: {{store.count}} 
+    <button @click="decrement">-</button>
+  </div>
+
+  <br><br>
+  <div>{{ stringStore.string }}</div>
+  <button @click="updateString">Update</button>
+
+</template>
